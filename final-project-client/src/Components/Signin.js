66f522a99a0c1e855a5df6edbdcc78e61db05b82
Avinaash <akri235@aucklanduni.ac.nz>
2023-04-11 11:33:17 -0400

    Working on sign-in

--- final-project-client/src/Components/Signin.js
+++ final-project-client/src/Components/Signin.js
@@ -1,8 +1,23 @@
- function Signin () {
+import './Signin.css'
+
+function Signin () {
      return (
-         <div>
-             Sign In Page
-         </div>
+        //creates a form that a user can sign in with a username and a password
+        <div class="container">
+            <form class="form" id="login">
+                <h1 class="form__title">Login</h1>
+                <div class="form__message form__message--error">Incorrect username/password combination</div>
+                <div class="form__input-group">
+                    <input type="text" class="form__input" autofocus placeholder="Username" />
+                    <div class="form__input-error-message">This is an error message</div>
+                </div>
+                <div class="form__input-group">
+                    <input type="password" class="form__input" autofocus placeholder="Password" />
+                    <div class="form__input-error-message"></div>
+                </div>
+                <button class="form__button" type="submit">Continue</button>
+            </form>
+        </div>
      )
  }
 
