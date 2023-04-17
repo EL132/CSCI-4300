bbd5909523dd297faebfe13ef1756870abee875c
Avinaash <akri235@aucklanduni.ac.nz>
2023-04-17 11:45:38 -0400

    using the form-hidden class

--- final-project-client/src/Components/Signin.js
+++ final-project-client/src/Components/Signin.js
@@ -17,8 +17,33 @@ function Signin () {
                 </div>
                 <button class="form__button" type="submit">Continue</button>
             </form>
+            <form class="form" id="createAccount">
+                <h1 class="form__title">Create Account</h1>
+                <div class="form__message form__message--error"></div>
+                <div class="form__input-group">
+                    <input type="text" class="form__input" autofocus placeholder="Username" />
+                    <div class="form__input-error-message"></div>
+                </div>
+                <div class="form__input-group">
+                    <input type="text" class="form__input" autofocus placeholder="Email Address" />
+                    <div class="form__input-error-message"></div>
+                </div>
+                <div class="form__input-group">
+                    <input type="password" class="form__input" autofocus placeholder="Password" />
+                    <div class="form__input-error-message"></div>
+                </div>
+                <div class="form__input-group">
+                    <input type="password" class="form__input" autofocus placeholder="Confirm Password" />
+                    <div class="form__input-error-message"></div>
+                </div>
+                <button class="form__button" type="submit">Continue</button>
+            </form>
         </div>
      )
+
+     document.addEventListener("DOMContentLoaded", () => {
+        const loginForm = document.querySelector(#login);
+     });
  }
 
 export default Signin;
