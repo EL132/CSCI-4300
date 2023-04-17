f73e58a8082c7d8435c8cc6bfdbdcc72e4f54bfe
Avinaash <akri235@aucklanduni.ac.nz>
2023-04-17 12:17:04 -0400

    Update Signin.js

--- final-project-client/src/Components/Signin.js
+++ final-project-client/src/Components/Signin.js
@@ -4,7 +4,7 @@ function Signin () {
      return (
         //creates a form that a user can sign in with a username and a password
         <div class="container">
-            <form class="form" id="login">
+            <form class="form form--hidden" id="login">
                 <h1 class="form__title">Login</h1>
                 <div class="form__message form__message--error"></div>
                 <div class="form__input-group">
@@ -16,6 +16,12 @@ function Signin () {
                     <div class="form__input-error-message"></div>
                 </div>
                 <button class="form__button" type="submit">Continue</button>
+                <p class="form__text">
+                    <a href="#" class="form__link">Forgot your password?</a>
+                </p>
+                <p class="form__text">
+                    <a href="#" class="form__link" href="./" id="linkCreateAccount">Don't have an account? Create Account</a>
+                </p>
             </form>
             <form class="form" id="createAccount">
                 <h1 class="form__title">Create Account</h1>
@@ -37,13 +43,31 @@ function Signin () {
                     <div class="form__input-error-message"></div>
                 </div>
                 <button class="form__button" type="submit">Continue</button>
+                <p class="form__text">
+                    <a class="form__link" href="./" id="linkLogin">Already have an account? Login</a>
+                </p>
             </form>
         </div>
      )
 
      document.addEventListener("DOMContentLoaded", () => {
-        const loginForm = document.querySelector(#login);
+        const loginForm = document.querySelector("#login");
+        const createAccountForm = document.querySelector("#createAccount");
+    
+        document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+            e.preventDefault();
+            loginForm.classList.add("form--hidden");
+            createAccountForm.classList.remove("form--hidden");
+        });
+    
+        document.querySelector("#linkLogin").addEventListener("click", e => {
+            e.preventDefault();
+            loginForm.classList.remove("form--hidden");
+            createAccountForm.classList.add("form--hidden");
+        });
      });
+
+     
  }
 
 export default Signin;
