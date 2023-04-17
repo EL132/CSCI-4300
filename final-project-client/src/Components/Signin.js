082d8a5bad179057f835e1012bd37689e1fc609f
Avinaash <akri235@aucklanduni.ac.nz>
2023-04-17 11:29:27 -0400

    added more css properties

--- final-project-client/src/Components/Signin.js
+++ final-project-client/src/Components/Signin.js
@@ -6,10 +6,10 @@ function Signin () {
         <div class="container">
             <form class="form" id="login">
                 <h1 class="form__title">Login</h1>
-                <div class="form__message form__message--error">Incorrect username/password combination</div>
+                <div class="form__message form__message--error"></div>
                 <div class="form__input-group">
                     <input type="text" class="form__input" autofocus placeholder="Username" />
-                    <div class="form__input-error-message">This is an error message</div>
+                    <div class="form__input-error-message"></div>
                 </div>
                 <div class="form__input-group">
                     <input type="password" class="form__input" autofocus placeholder="Password" />
